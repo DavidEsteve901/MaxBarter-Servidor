41141597ad7893f5f7ce04ad5aad69925c263395
David Esteve <desteve@vidal-ac.es>
2022-04-12 17:04:12 +0200

    pequeño cambio, poniendo status a respuestas

--- app/controllers/auth.controller.js
+++ app/controllers/auth.controller.js
@@ -37,7 +37,7 @@ export const registro = async (req, res) => {
             res.status(200).json({token})
             // res.json(user);
         }else{
-            res.json("Usuario ya existe")
+            res.status(401).json({message: "El nombre de usuario ya existe"})
         }
         
         
