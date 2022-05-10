43c3da17a17c4c8f32b85238d501649fd29a70b7
David Esteve <desteve@vidal-ac.es>
2022-05-10 12:20:19 +0200

    Creo más productos

--- app/database/seeders/20220418125522-create-productos.js
+++ app/database/seeders/20220418125522-create-productos.js
@@ -81,6 +81,72 @@ module.exports = {
         user: "pedro",
         tipo: 7
       }),
+      Producto.create({
+        id:10,
+        titulo: "Air Jordan 2 Low x Off-White",
+        descripcion: "Air Jordan 2 Low x Off-White Black and Varsity Royal Se han conseguido en sorteo de nike SNKRS , se envian con doble caja y factura de compra. Talla 43 se entrega en mano en todo Madrid - Se escuchan ofertas serias-",
+        match: false,
+        user: "pablo",
+        tipo: 11
+      }),
+      Producto.create({
+        id:11,
+        titulo: "Zapatillas Nike hombre",
+        descripcion: "Zapatillas Nike talla 42,muy buen estado. Poco usadas. Perfectas  para uso cotidiano. Hago envios",
+        match: false,
+        user: "pablo",
+        tipo: 11
+      }),
+      Producto.create({
+        id:12,
+        titulo: "Mammut decoración",
+        descripcion: "Mammut decorativo muy bonito con un rotito como se ve en las fotos",
+        match: false,
+        user: "pablo",
+        tipo: 12
+      }),
+      Producto.create({
+        id:13,
+        titulo: "Conga1.090 cecotec",
+        descripcion: "Aspirador perfecto para eliminar el pelo que suelta las mascotas, aspira y friega a la vez, tiene 2 cepillos centrales intercambiables y se carga sola.",
+        match: false,
+        user: "pablo",
+        tipo: 13
+      }),
+      Producto.create({
+        id:14,
+        titulo: "Pelele bebé 12 meses",
+        descripcion: "Sin estrenar, no etiqueta.",
+        match: false,
+        user: "alba",
+        tipo: 15
+      }),
+      Producto.create({
+        id:15,
+        titulo: "Top crochet T/S",
+        descripcion: "Top de punto con calados crochet ganchillo rosa lila malva lavanda Talla S Nuevo con etiquetas",
+        match: false,
+        user: "alba",
+        tipo: 11
+      }),
+      Producto.create({
+        id:16,
+        titulo: "Top volantes T/S",
+        descripcion: "Top cropped verde con volantes Talla S Nuevo Con etiquetas",
+        match: false,
+        user: "alba",
+        tipo: 11
+      }),
+      Producto.create({
+        id:17,
+        titulo: "Rizador Tenaza SURKER ",
+        descripcion: "Rizador y tenaza en perfecto estado casi sin uso.",
+        match: false,
+        user: "alba",
+        tipo: 13
+      }),
+      
+  
     ])
   },
 
--- app/database/seeders/20220418126155-create-img.js
+++ app/database/seeders/20220418126155-create-img.js
@@ -38,6 +38,35 @@ module.exports = {
       {url:"user3-producto2-1.jpeg",producto:"9"},
       {url:"user3-producto2-2.jpeg",producto:"9"},
       {url:"user3-producto2-3.jpeg",producto:"9"},
+
+      {url:"user4-producto1-1.jpeg",producto:"10"},
+      {url:"user4-producto1-2.jpeg",producto:"10"},
+      {url:"user4-producto1-3.jpeg",producto:"10"},
+      {url:"user4-producto1-4.jpeg",producto:"10"},
+
+      {url:"user4-producto2-1.jpeg",producto:"11"},
+      {url:"user4-producto2-2.jpeg",producto:"11"},
+      {url:"user4-producto2-3.jpeg",producto:"11"},
+
+      {url:"user4-producto3-1.jpeg",producto:"12"},
+      {url:"user4-producto3-2.jpeg",producto:"12"},
+      {url:"user4-producto3-3.jpeg",producto:"12"},
+
+      {url:"user4-producto4-1.jpeg",producto:"13"},
+      {url:"user4-producto4-2.jpeg",producto:"13"},
+
+      {url:"user5-producto1-1.jpeg",producto:"14"},
+      {url:"user5-producto1-2.jpeg",producto:"14"},
+
+      {url:"user5-producto2-1.jpeg",producto:"15"},
+      {url:"user5-producto2-2.jpeg",producto:"15"},
+
+      {url:"user5-producto3-1.jpeg",producto:"16"},
+
+      {url:"user5-producto4-1.jpeg",producto:"17"},
+      {url:"user5-producto4-2.jpeg",producto:"17"},
+      {url:"user5-producto4-3.jpeg",producto:"17"},
+
     
 
 
